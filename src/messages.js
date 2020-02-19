ce3e9a44dfef0359a24d28813e5a8eb44a416def
Guilherme Moura <gmoura.nas@gmail.com>
2020-02-18 21:53:19 -0300

    chore(test1): test1

--- src/messages.js
+++ src/messages.js
@@ -1,7 +1,8 @@
 export default {
   en: {
     'hello.world': 'Hello world',
-    'load.gifs': 'Load Gifs'
+    'load.gifs': 'Load Gifs',
+    'load.gifs1': 'Load Gifs'
   },
   'pt-BR': {
     'hello.world': 'Oi mundo',
